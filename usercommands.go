caa28d9b252e40738132ce1873e3641ea100b5cc
maxisvk <massirgm@gmail.com>
2020-06-25 17:37:47 +0200

    Update usercommands.go

--- usercommands.go
+++ usercommands.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"io"
 
-	"github.com/andlabs/reallymine/command"
-	"github.com/andlabs/reallymine/disk"
-	"github.com/andlabs/reallymine/kek"
+	"github.com/maxisvk/reallymine/command"
+	"github.com/maxisvk/reallymine/disk"
+	"github.com/maxisvk/reallymine/kek"
 )
 
 func runUntilDEK(d *disk.Disk, out io.Writer) (dec *Decrypter, err error) {
